3f89d87e406ddba8de213651c940deddbc1cb2dc
Barbarpotato <darmawanjr88@gmail.com>
2024-07-08 22:23:02 +0800

    replace browserrouter to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
@@ -7,10 +7,10 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider theme={theme}>
-    <BrowserRouter>
+    <HashRouter>
       <div className='App' id='App'>
         <App />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </ChakraProvider>
 )
