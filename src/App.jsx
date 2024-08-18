f0f53553ec3fc2eb415d1ee6e045ce025e0f8735
Barbarpotato <darmawanjr88@gmail.com>
2024-08-18 19:17:36 +0800

    reduce main thread works (3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Fragment } from 'react';
-import Index from './pages/main/index'
-import Labs from './pages/labs'
-import LabContentDetail from './pages/labs/detail';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Fragment, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Loading from './components/loading/index';
 
-const queryClient = new QueryClient()
+const Index = lazy(() => import('./pages/main/index'));
+const Labs = lazy(() => import('./pages/labs'));
+const LabContentDetail = lazy(() => import('./pages/labs/detail'));
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Fragment>
       <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/labs" element={<Labs />} />
-          <Route path='/lab/:blogId' element={<LabContentDetail />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/labs" element={<Labs />} />
+            <Route path='/lab/:blogId' element={<LabContentDetail />} />
+          </Routes>
+        </Suspense>
       </QueryClientProvider>
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
