78e6d668e1a479c4911e2b33aa77c7d040ebea04
Barbarpotato <darmawanjr88@gmail.com>
2024-12-30 09:17:44 +0800

    use caching for client token reduce request

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import { lazy, Suspense, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ChakraProvider } from '@chakra-ui/react';
-import { RequestResource } from './api/PROXY';
 
-import Loading from './components/Loading'; // Ensure this component exists and is functional
+import Main from './pages/Main';
 
 import './index.css'; // Ensure this file exists and has valid styles
 
-// Initialize React Query Client
 const queryClient = new QueryClient();
 
-const Portfolio = lazy(() => import("site_registry/Portfolio"));
-
-// App Component
 function App() {
-
-  const [token, setToken] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(true);
-    RequestResource()
-      .then((data) => setToken(data.token))
-      .catch((error) => console.error(error));
-    setIsLoading(false);
-  }, [])
-
-  if (isLoading || !token) {
-    return <Loading />;
-  }
-
   return (
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
-        <Suspense fallback={<Loading />}>
-          <Portfolio token={token} />
-        </Suspense>
+        <Main />
       </QueryClientProvider>
     </ChakraProvider>
-  )
+  );
 }
 
 // Render the App
