589b9492a889a21cf4d2eb76289fc2fe14684e39
Barbarpotato <darmawanjr88@gmail.com>
2025-02-02 20:15:26 +0800

    fix ci/cd pipeline

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/barbarpotato.github.io/",
+  base: "/",
   plugins: [react()],
   esbuild: {
     loader: "jsx",
